a7378f492f37a91f8c97fdaa3dd4ead10aeca82c
alacasalimu <a.mutekin@gmail.com>
2024-10-18 15:55:54 +0300

    initial commit

---
+++ jira-clone/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+import { TestComponent } from "@/features/test";
+export default function Home() {
+  return (
+    <div>
+      <TestComponent></TestComponent>
+    </div>
+  );
+}
