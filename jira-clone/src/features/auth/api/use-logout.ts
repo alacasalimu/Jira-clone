8c1bca296066a4178ab73d466127139f77ce5300
alacasalimu <a.mutekin@gmail.com>
2024-10-26 19:01:19 +0300

    Builded a session middleware

--- jira-clone/src/features/auth/api/use-logout.ts
+++ jira-clone/src/features/auth/api/use-logout.ts
@@ -4,18 +4,15 @@ import { InferRequestType, InferResponseType } from "hono";
 import { client } from "@/lib/rpc";
 import { useRouter } from "next/navigation";
 
-type ResponseType = InferResponseType<(typeof client.api.auth.login)["$post"]>;
-type RequestType = InferRequestType<(typeof client.api.auth.login)["$post"]>;
+type ResponseType = InferResponseType<(typeof client.api.auth.logout)["$post"]>;
 
-export const useLogin = () => {
-  const queryClient = useQueryClient();
+export const useLogout = () => {
   const router = useRouter();
+  const queryClient = useQueryClient();
 
-  const mutation = useMutation<ResponseType, Error, RequestType>({
-    mutationFn: async ({ json }) => {
-      const response = await client.api.auth.login["$post"]({
-        json,
-      });
+  const mutation = useMutation<ResponseType, Error>({
+    mutationFn: async () => {
+      const response = await client.api.auth.logout["$post"]();
       return await response.json();
     },
     onSuccess: () => {
