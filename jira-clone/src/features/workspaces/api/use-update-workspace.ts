82d565c0a8b041dc5534704a5232789e974b00f3
alacasalimu <a.mutekin@gmail.com>
2024-11-01 16:18:18 +0300

    Builded workspace settings

--- jira-clone/src/features/workspaces/api/use-update-workspace.ts
+++ jira-clone/src/features/workspaces/api/use-update-workspace.ts
@@ -5,27 +5,34 @@ import { InferRequestType, InferResponseType } from "hono";
 import { client } from "@/lib/rpc";
 import { useRouter } from "next/navigation";
 
-type ResponseType = InferResponseType<(typeof client.api.workspaces)["$post"]>;
-type RequestType = InferRequestType<(typeof client.api.workspaces)["$post"]>;
+type ResponseType = InferResponseType<
+  (typeof client.api.workspaces)[":workspaceId"]["$patch"],
+  200
+>;
+type RequestType = InferRequestType<
+  (typeof client.api.workspaces)[":workspaceId"]["$patch"]
+>;
 
-export const useCreateWorkspace = () => {
+export const useUpdateWorkspace = () => {
   const queryClient = useQueryClient();
 
   const mutation = useMutation<ResponseType, Error, RequestType>({
-    mutationFn: async ({ form }) => {
-      const response = await client.api.workspaces["$post"]({
+    mutationFn: async ({ form, param }) => {
+      const response = await client.api.workspaces[":workspaceId"]["$patch"]({
         form,
+        param,
       });
 
       if (!response.ok) {
-        throw new Error("Failed to create workspace");
+        throw new Error("Failed to update workspace");
       }
 
       return await response.json();
     },
     onSuccess: ({ data }) => {
-      toast.success("Workspace created");
+      toast.success("Workspace updated");
       queryClient.invalidateQueries({ queryKey: ["workspaces"] });
+      queryClient.invalidateQueries({ queryKey: ["workspaces", data.$id] });
     },
     onError: () => {
       toast.error("Failed to create workspace");
