e3b30d7319e1514c23dfdba033af19bbeb2af06b
alacasalimu <a.mutekin@gmail.com>
2024-11-04 19:35:15 +0300

    Builded the invite system

--- jira-clone/src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
+++ jira-clone/src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -1,5 +1,5 @@
-import { getCurrent } from "@/features/auth/queries";
 import { redirect } from "next/navigation";
+import { getCurrent } from "@/features/auth/queries";
 
 const WorkspaceIdPage = async () => {
   const user = await getCurrent();
