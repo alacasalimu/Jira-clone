25fda82cbc161157e3ad5a65373827c4aa8eca21
alacasalimu <a.mutekin@gmail.com>
2024-11-14 18:29:49 +0300

    Builded a data kanban

--- jira-clone/src/features/tasks/components/task-view-switcher.tsx
+++ jira-clone/src/features/tasks/components/task-view-switcher.tsx
@@ -17,6 +17,7 @@ import { DataTable } from "./data-table";
 import { useGetTasks } from "../api/use-get-tasks";
 import { useCreateTaskModal } from "../hooks/use-create-task-modal";
 import { useTaskFilters } from "../hooks/use-task-filters";
+import { DataKanban } from "./data-kanban";
 
 export const TaskViewSwitcher = () => {
   const [{ status, assigneeId, projectId, dueDate }] = useTaskFilters();
@@ -73,7 +74,7 @@ export const TaskViewSwitcher = () => {
               <DataTable columns={columns} data={tasks?.documents ?? []} />
             </TabsContent>
             <TabsContent value="kanban" className="mt-0">
-              {JSON.stringify(tasks)}
+              <DataKanban data={tasks?.documents ?? []} />
             </TabsContent>
             <TabsContent value="calendar" className="mt-0">
               {JSON.stringify(tasks)}
